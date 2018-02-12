5ba44fbff58458b7e566916a9ad42f0f70677179
bugariu bogdan <bbugariu@protonmail.com>
2018-02-12 19:45:17 +0200

    Set up home controller and install security package

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bbugariu
+ * Date: 12.02.2018
+ * Time: 19:39
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return new Response("Homepage");
+    }
+}
